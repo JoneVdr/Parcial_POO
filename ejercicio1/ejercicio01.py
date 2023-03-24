7a4a51abb04a99ba23db320009fa3749d25b6bea
jone <jvegadel@myuax.com>
2023-03-24 10:49:29 +0100

    .

--- ejercicio1/ejercicio01.py
+++ ejercicio1/ejercicio01.py
@@ -6,11 +6,13 @@ class Libro:
         self.autor = autor
         self.paginas = paginas
         self.genero = genero
+        self.año_publicacion = año_publicacion
 
     def libro1(self):
         titulo = input ("Introduzca el titulo del libro: ")
         autor = input ("Introduzca el autor del libro: ")
         paginas = int(input ("Introduzca el numero de paginas del libro: "))
         genero = input ("Introduzca el genero del libro: ")
+        año_publicacion = input ("Introduzca el año de publicacion del libro: ")
 
 print(Libro)
